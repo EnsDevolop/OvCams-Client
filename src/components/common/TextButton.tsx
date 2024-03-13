eb6fedeb3b70f41c49b29226cfa04d356a701bb4
EnsDevelop <chemins60217@gmail.com>
2024-03-13 20:16:15 +0900

    fix: add missing attribute

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -5,7 +5,10 @@ import React from "react";
 import useFilteredProps from "@/libs/hooks/useFilteredProps";
 import { keyframes } from "@emotion/react";
 
-type IconProps = { startIcon?: React.ReactNode; endIcon?: React.ReactNode };
+type IconProps = {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+};
 type OtherProps = { color: string; sz: string; active: boolean };
 type TextButtonProps = ButtonPropsProps & IconProps & OtherProps;
 
@@ -22,7 +25,7 @@ const included = [
 
 const TextButton = React.forwardRef<HTMLButtonElement, TextButtonProps>(
   function TextButton(props, ref) {
-    const { color, startIcon, endIcon, disabled, children, active } = props;
+    const { color, startIcon, endIcon, disabled, children, active, sz } = props;
 
     const others = useFilteredProps(props, included) as Omit<
       ButtonPropsProps,
@@ -31,7 +34,7 @@ const TextButton = React.forwardRef<HTMLButtonElement, TextButtonProps>(
     return (
       <TextButtonBox
         color="white"
-        sz="medium"
+        sz={sz}
         active={active}
         ref={ref}
         {...others}
@@ -65,7 +68,7 @@ const BorderBottomKeyFrame = keyframes`
 const TextButtonBox = styled(Button)<OtherProps>`
   color: ${themedPalette.white};
   padding: 1px 2px;
-  font-size: 18px;
+  font-size: ${({ sz }) => (sz === "small" ? "14px" : "18px")};
   font-weight: ${({ active }) => (active ? 600 : 400)};
   font-family: Roboto;
   line-height: 20px;
