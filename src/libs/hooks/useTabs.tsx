45255cca80f71ae687aa360a397dded7f6aa9166
EnsDevelop <chemins60217@gmail.com>
2024-04-01 20:15:20 +0900

    docs: remove condition

--- src/libs/hooks/useTabs.tsx
+++ src/libs/hooks/useTabs.tsx
@@ -2,9 +2,6 @@ import { useState } from "react"
 
 export default function useTabs(initialTab: number, allTabs: React.ReactNode[]) {
   const [currentIndex, setCurrentIndex] = useState(initialTab)
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return
-  }
 
   return [allTabs[currentIndex], setCurrentIndex] as [React.ReactNode, typeof setCurrentIndex]
 }
