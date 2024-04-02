01b886b456e69097269a92e847e2306b028f6597
EnsDevelop <chemins60217@gmail.com>
2024-04-03 08:18:44 +0900

    feat: add index value

--- src/libs/hooks/useTabs.tsx
+++ src/libs/hooks/useTabs.tsx
@@ -3,5 +3,5 @@ import { useState } from "react"
 export default function useTabs(initialTab: number, allTabs: React.ReactNode[]) {
   const [currentIndex, setCurrentIndex] = useState(initialTab)
 
-  return [allTabs[currentIndex], setCurrentIndex] as [React.ReactNode, typeof setCurrentIndex]
+  return [currentIndex, allTabs[currentIndex], setCurrentIndex] as [number, React.ReactNode, typeof setCurrentIndex]
 }
