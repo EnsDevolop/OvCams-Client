9bc54e59d4fb5e07f2075ec05a3d347e0d38205a
EnsDevelop <chemins60217@gmail.com>
2024-04-07 08:52:55 +0900

    feat: add header user

--- src/libs/apis/auth/index.ts
+++ src/libs/apis/auth/index.ts
@@ -1,5 +1,7 @@
 import { customCookie } from "@/libs/CustomCookie"
 import axios from "axios"
+import { instance } from "../axios"
+import { useQuery } from "@tanstack/react-query"
 
 export const useReissue = (refreshToken: string) => {
   axios
@@ -10,4 +12,12 @@ export const useReissue = (refreshToken: string) => {
     })
     .then((e) => customCookie.set.token(e.data.accessToken, e.data.refreshToken))
     .catch((e) => console.log(e))
+}
+
+export const useProfileQuery = () => {
+  const response = async () => {
+    const { data } = await instance.get("/auth/profile")
+    return data
+  }
+  return useQuery({ queryKey: ["profile"], queryFn: response, enabled: !!customCookie.get.accessToken() })
 }
