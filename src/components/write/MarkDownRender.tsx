ed13481680197a8b8daed98393c70dc36eaf0bd7
EnsDevelop <chemins60217@gmail.com>
2024-04-10 08:52:16 +0900

    design: change style

--- src/components/write/MarkDownRender.tsx
+++ src/components/write/MarkDownRender.tsx
@@ -3,5 +3,10 @@ import { marked } from "marked"
 export default function MarkDownRender({ mark }: { mark: string }) {
   const html = marked.parse(mark)
 
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>
+  return (
+    <div
+      style={{ width: "50%", height: "600px", overflowWrap: "break-word" }}
+      dangerouslySetInnerHTML={{ __html: html }}
+    ></div>
+  )
 }
